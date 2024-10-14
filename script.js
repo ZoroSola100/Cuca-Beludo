bc9733ee91fa6e2524070fb0a8e9ebde61ee87ee
ZoroSola100 <urbanogabriel265@gmail.com>
2024-10-14 14:23:08 -0300

    tetyghkjlç
    fghjk

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('comprar-btn').addEventListener('click', function() {
+    // Redireciona para o site especificado
+    window.location.href = 'http://www.republiquedesmangues.fr/';
+});
